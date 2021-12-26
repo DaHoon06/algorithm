772bfb5763b106a0a49693072e92f50490bbc295
dahoon06 <dahoon226@naver.com>
2021-12-26 19:53:15 +0900

    #6 포켓몬
    "체육복"은 다시 생각해보자...

--- programmers/javascript/레벨1/체육복/index.js
+++ programmers/javascript/레벨1/체육복/index.js
@@ -5,14 +5,23 @@
  * @param reserve: 여벌의 체육복을 가진 학생 수
  * @returns {number}: 체육을 듣는 학생의 수 최댓값
  */
-function solution(n, lost, reserve) {
-    let result = 0;
+function solution_error(n, lost, reserve) {
+    let lostStudent = lost.filter(a => !reserve.includes(a));
+    let reserveStudent = reserve.filter(a => !lost.includes(a));
 
-    for(let i = 0; i < n.length; i++){
+    return n - lostStudent.filter(lostStudent => {
+        let extra = reserveStudent.find(reserveStudent => Math.abs(reserveStudent - lostStudent))
+        if(!extra) return true;
 
-    }
+        reserveStudent = reserveStudent.filter(reserveStudent => reserveStudent !== extra);
+    }).length;
+}// 이 solution 틀렸다고 나옴;;;
 
 
+function solution(n, lost, reserve){
+    let lostStudents = lost.filter(a => reserve.includes(a));
+    let reserveStudents = reserve.filter(a => lost.includes(a));
 
-    return result;
+
+    return
 }
---
+++ programmers/javascript/레벨1/포켓몬/index.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @param nums: 포켓몬 번호가 담긴 배열
+ */
+function solution(nums) {
+    let result = [];
+    let N = nums.length; // 총 포켓몬
+    let choice = N / 2;  // 내가 선택 할 수 있는 포켓몬 수
+
+    for(let i = 0; i < N; i++){
+        if(result.length < choice) {
+            if(!result.includes(nums[i])){
+                result.push(nums[i]);
+            }
+        }
+    }
+
+    return result.length;
+}
+//아 SET...
+function solution2(nums) {
+    const max = nums.length/2;
+    const arr = [...new Set(nums)];
+
+    return arr.length > max ? max : arr.length
+}
+function solution3(nums){
+    let answer = [...new Set(nums)]
+    let limit = nums.length/2;
+
+    return answer.length > limit ? limit : answer.length;
+}
