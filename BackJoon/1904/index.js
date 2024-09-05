db7d5f3a6f674b7e99eae1b54607e1dca6cc8385
dahoon06 <dahoon226@naver.com>
2024-09-05 21:20:15 +0900

    # 백업

---
+++ BackJoon/1904/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("BackJoon/1904/input.txt").toString().split(" ");
+
+const size = +input[0];
+
+
+
+for (let i = 0; i < size; i++) {
+
+}
---
+++ BackJoon/1991/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync("BackJoon/1991/input.txt").toString().split(" ");
+
+const size = +input[0];
+
+
