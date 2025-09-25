4f40813474f9d1a4ed018828fc9f739773bd5de9
DaHoon Jeon <dahoon226@naver.com>
2025-09-25 22:03:02 +0900

    [Bronze V] Title: A+B - 5, Time: 104 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+for (let i = 0; i < input.length - 1; i++) {
+  const [a, b] = input[i].split(" ");
+  console.log(+a + +b);
+}
